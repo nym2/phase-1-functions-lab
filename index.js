d02f53932351e306195eff42c16656db3f37c74a
Nympha Pamba <pambapraise@gmail.com>
2024-07-05 13:23:16 +0300

    adds .results.json; index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqlocation = 42;
+    return Math.abs(someValue - hqlocation);
+}
+
+function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feetPerBlock = 264;
+    return (blocks * feetPerBlock)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start);
+    const feetPerBlock = 264;
+    return (blocksTravelled * feetPerBlock)
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination)
+
+    if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+    else if (distance > 2000) {
+        return 25;
+    }
+    else if (distance > 400) {
+        return ( distance - 400) * 0.02;
+    }
+    else {
+        return 0;
+    }
+}
